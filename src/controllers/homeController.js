3eba478bc13f1877aec6bc654c8653ebf94e834f
404-Hai <thanhhai691999@gmail.com>
2022-04-04 21:44:56 +0700

    Update file homeController

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,6 @@
 import db from '../models/index'
+import CRUDService from '../services/CRUDService'
+//Get
 let getHomePage = async(req, res) => {
     try{
         let data = await db.User.findAll();
@@ -11,9 +13,17 @@ let getHomePage = async(req, res) => {
     }
     
 }
-
 let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
+}
+let getCRUD = (req,res)=>{
+    return res.render('crud.ejs')
+}
+//Post
+let postCRUD = async (req, res)=>{
+    let message = await CRUDService.createNewUser(req.body)
+    console.log(message)
+    return res.render('test/about.ejs')
 }
 
 // object: {
@@ -22,5 +32,7 @@ let getAboutPage = (req, res) => {
 // }
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD
 }
